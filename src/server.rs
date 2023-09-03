0392b7d907c3f53b9715e6551f8c3f3e614abff4
Apachedrag427 <93100547+Apachedrag427@users.noreply.github.com>
2023-09-02 21:35:08 -0500

    Switch server port amogus cloudflare

--- src/server.rs
+++ src/server.rs
@@ -34,8 +34,8 @@ pub mod server {
 	}
 
 	pub fn start_listening() {
-		let server = Arc::new(Server::http("0.0.0.0:7878").unwrap());
-		println!("Listening on port 7878");
+		let server = Arc::new(Server::http("0.0.0.0:443").unwrap());
+		println!("Listening on port 443");
 
 		let mut handles = Vec::new();
 
